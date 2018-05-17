7295d7d97867f08af63d60ec00a6c8b949a7d47e
CHristelle <CHristelle@DESKTOP-MV2QIEM>
2018-05-17 09:03:09 -0400

    fin del capitulo 5

---
+++ EjerciciosLibroCsharp/Capitulo5/ejercicios4_5.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EjerciciosLibroCsharp.Capitulo5
+{
+    class ejercicios4_5
+    {
+        public void cap5_4()
+        {
+            int numero, factorial = 1;
+
+            Console.WriteLine("digite un numero");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int x = 1; x < numero + 1; x++)
+            {
+                factorial = factorial * x;
+            }
+            Console.Write("el resultado es : " + factorial);
+
+            Console.ReadKey();
+        }
+
+        public void cap5_5()
+        {
+            int numero;
+            Console.WriteLine("digite un numero");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero == 1)
+            {
+                Console.Write("Clifford Jeffrey");
+
+            }
+            else if (numero == 2)
+            {
+                Console.Write("Danilo Medina");
+            }
+            else if (numero == 3)
+            {
+                Console.Write("Programacion Aplicada 1");
+            }
+            else if (numero == 4)
+            {
+                Console.Write("Republica Dominicana");
+            }
+            else if (numero > 4)
+            {
+                Console.WriteLine("ya no hay mas Palabra solo cuatro Palabra");
+
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+
--- EjerciciosLibroCsharp/Program.cs
+++ EjerciciosLibroCsharp/Program.cs
@@ -9,6 +9,42 @@ namespace EjerciciosLibroCsharp
     {
         static void Main(string[] args)
         {
+
+            string linea;
+            for (; ; )
+            {
+                Console.WriteLine("Aqui tenemos todos los ejercicios del capitulo 5 hastas el capitulo 8");
+                Console.WriteLine("Escoge una opcion: ");
+                Console.WriteLine("1- Capitulo 5 ");
+                Console.WriteLine("2- Capitulo 6 ");
+                Console.WriteLine("3- Capitulo 7 ");
+                Console.WriteLine("4- Capitulo 8 ");
+                linea = Console.ReadLine();
+                Console.Clear();
+                switch (linea)
+                {
+                    case "1":
+                        Console.WriteLine("1- factorial de un numero");
+                        Console.WriteLine("2- cadena que regresa palabras");
+                        linea = Console.ReadLine();
+                        Console.Clear();
+                        if (linea == "1")
+                        {
+                            EjerciciosLibroCsharp.Capitulo5.ejercicios4_5 E = new EjerciciosLibroCsharp.Capitulo5.ejercicios4_5();
+                            E.cap5_4();
+
+                        }
+                        else
+                            if (linea == "2")
+                        {
+                            EjerciciosLibroCsharp.Capitulo5.ejercicios4_5 E = new EjerciciosLibroCsharp.Capitulo5.ejercicios4_5();
+                            E.cap5_5();
+                        }
+                        Console.Clear();
+
+                        break;
+                }
+            }
         }
     }
 }
